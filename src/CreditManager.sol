c112d37ef706197db518a9cf5bc181c8f0b8796a
kevinsslin <kk123750964@gmail.com>
2023-08-04 01:05:31 +0800

    add CreditManager and ICreditManager

---
+++ src/CreditManager.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.19;
+
+import { ICreditManager } from "./interfaces/ICreditManager.sol";
+
+contract CreditManager is ICreditManager {
+    
+}
