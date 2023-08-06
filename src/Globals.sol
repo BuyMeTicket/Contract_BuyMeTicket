7d6417579f989ce672362959bf2ac5a5e49727ff
kevinsslin <kk123750964@gmail.com>
2023-08-06 17:08:15 +0800

    add some basic feature for Globals.sol

--- src/Globals.sol
+++ src/Globals.sol
@@ -4,8 +4,15 @@ pragma solidity 0.8.19;
 import {IGlobals} from "./interfaces/IGlobals.sol";
 
 contract Globals is IGlobals {
-    //** Storage Functions - for storing data */
+    //** Storage */
+    address public override governor;
     mapping(address => bool) public isEventHolders;
+    uint256 public override createEventFee = 0 ether;
+
+    constructor(address _governor) {
+        require(_governor != address(0), "Globals: _governor is zero address");
+        governor = _governor;
+    }
 
     //** allow list function */
     function setValidEventHolder(address _eventHolder, bool _isValid) external override {
@@ -14,8 +21,27 @@ contract Globals is IGlobals {
         emit ValidEventHolderSet(_eventHolder, _isValid);
     }
 
+    //** Setter Functions */
+    function setCreateEventFee(uint256 _fee) external override onlyGovernor {
+        createEventFee = _fee;
+        emit CreateEventFeeSet(_fee);
+    }
+
+    //** Governor Transfer Functions */
+    function transferGovernor(address _newGovernor) external override onlyGovernor {
+        require(_newGovernor != address(0), "Globals: _newGovernor is zero address");
+        emit GovernorTransferred(governor, _newGovernor);
+        governor = _newGovernor;
+    }
+
     //** view function */
     function isValidEventHolder(address _eventHolder) external view override returns (bool) {
         return isEventHolders[_eventHolder];
+    }
+
+    //** Modifier */
+    modifier onlyGovernor() {
+        require(msg.sender == governor, "Globals: only governor");
+        _;
     }
 }
--- src/interfaces/IGlobals.sol
+++ src/interfaces/IGlobals.sol
@@ -5,12 +5,25 @@ interface IGlobals {
     //** events */
     event ValidEventHolderSet(address indexed eventHolder, bool indexed isValid);
 
+    event GovernorTransferred(address _previousGovernor, address _newGovernor);
+
+    event CreateEventFeeSet(uint256 _fee);
+
     //** allow list function */
 
     function setValidEventHolder(address _eventHolder, bool _isValid) external;
 
-    //** view function */
+    //** Setter Functions */
+    function setCreateEventFee(uint256 _fee) external;
 
-    // mapping(address => bool) public isEventHolders;
+    //** view function */
+    function governor() external view returns (address);
+
+    function createEventFee() external view returns (uint256);
+
     function isValidEventHolder(address _eventHolder) external view returns (bool);
+
+    //** Governor Transfer Functions */
+
+    function transferGovernor(address _newGovernor) external;
 }
