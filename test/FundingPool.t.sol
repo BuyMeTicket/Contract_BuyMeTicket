b59b4e8c06cf380d88b826751ca5813967b84871
kevinsslin <kk123750964@gmail.com>
2023-09-22 10:15:09 +0800

    add basic test for funding feature

--- test/FundingPool.t.sol
+++ test/FundingPool.t.sol
@@ -2,18 +2,26 @@
 pragma solidity ^0.8.19;
 
 import "./BaseTest.t.sol";
+import {FundingPool} from "../src/FundingPool.sol";
 
-contract FundingPoolFactoryTest is BaseTest {
+contract FundingPoolTest is BaseTest {
+    FundingPool public fundingPool;
+
     function setUp() public override {
         super.setUp();
-    }
 
-    function test_CreatePool() external {
         changePrank(EVENT_HOLDER);
         (address poolAddress, uint256 poolId) = fundingPoolFactory.createPool(
             address(usdt), block.timestamp + 1 days, block.timestamp + 31 days, 100_000e18
         );
-        assertEq(poolAddress, address(fundingPoolFactory.fundingPools(0)));
-        assertEq(poolId, 0);
+        fundingPool = FundingPool(poolAddress);
     }
+
+    function test_IsPoolOpen() external {
+        assertEq(fundingPool.isPoolOpen(), false);
+
+        vm.warp(block.timestamp + 1 days);
+        assertEq(fundingPool.isPoolOpen(), true);
+    }
+
 }
